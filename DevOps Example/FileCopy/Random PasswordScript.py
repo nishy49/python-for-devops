7f50df6ecc872bae214626f34a178ac190f2d7a2
Nischal Prasad Raman <ramannischal99@gmail.com>
2025-03-20 23:05:29 +0530

    Added new file called randm scripty.py

---
+++ DevOps Example/FileCopy/Random PasswordScript.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters =string.ascii_letters + string.digits +string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    print(f"Generated password: {password}")
+
+generate_password(16)
